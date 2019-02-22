38f95c82cd6eefc72613a209c95e79bf74812a36
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-02-22 23:17:14 +0100

    clean application and extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def new
     @article = Article.new
   end
@@ -13,16 +14,11 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = 'Article was updated successfully'
       redirect_to article_path(@article)
@@ -36,12 +32,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "article was successfully deleted"
     redirect_to articles_path
   end
+
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
