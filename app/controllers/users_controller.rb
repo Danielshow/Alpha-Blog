1aa9875ee46b164e9e52f08ced0c7f62ce9dca2e
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-02-23 20:03:07 +0100

    add pagination to all pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def index 
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def create
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
   private
 
