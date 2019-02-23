7c4352ec15faa230e4d6e5dde639c22728185d83
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-02-23 17:04:38 +0100

    add user data to article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase}
+
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: {minimum: 3, maximum: 25}
   
-  VALID_EMAIL_REGEX=  /^([a-z_.!@#$%^&*0-9]{3,25})@([a-z]{3,20})\.([a-z]){2,7}(\.[a-z]{2,5})?$/i
+  VALID_EMAIL_REGEX= /([a-z_.!@#$%^&*0-9]{3,25})@([a-z]{3,20})\.([a-z]){2,7}(\.[a-z]{2,5})?/i
   validates :email, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
